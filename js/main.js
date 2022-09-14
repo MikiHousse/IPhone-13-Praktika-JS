398b22c518f82e7e2d3edf38db731f6da1a90cdc
MikiHousse <mikichaoss@gmail.com>
2022-09-14 09:17:11 +0300

    Реализовал плавную прокрутку по странице с помощью навигации

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const links = document.querySelectorAll('.header-menu__item a');
+
+// seamless.polyfill();
+
+links.forEach((element, index, array) => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const id = element.getAttribute('href').substring(1);
+        const section = document.getElementById(id);
+
+        if (section) {
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        // } else {
+        //     console.log('nope');
+        //     // seamless.scrollIntoView(document.querySelector("#characteristics"), {
+        //     //     behavior: "smooth",
+        //     //     block: "center",
+        //     //     inline: "center",
+        //     };
+        }
+    })
+});
+
+const xar = document.querySelectorAll('.card-details__link-characteristics');
+
+xar.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const id = element.getAttribute('href').substring('#characteristics');
+        const section =document.querySelector(id);
+
+        if (section) {
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    })
+})
